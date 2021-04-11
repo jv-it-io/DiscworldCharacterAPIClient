c34b4c856303570d9b21f8b0432ed5514e0c3a63
john <vandersmissen.jonathan@gmail.com>
2021-04-11 16:16:17 +0200

    init

---
+++ src/main/java/be/jvit/discworldcharacterapiclient/DiscWorldCharacterApiClientApplication.java
@@ -0,0 +1,13 @@
+package be.jvit.discworldcharacterapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscWorldCharacterApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscWorldCharacterApiClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/jvit/discworldcharacterapiclient/DiscWorldCharacterApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jvit.discworldcharacterapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscWorldCharacterApiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
